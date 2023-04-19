55f5137162b19cf77aec0c9f5982c21c3b3db356
ts-geek43 <manish.prajapati@techstaunch.com>
2023-04-19 13:05:27 +0530

    todo display page updated

--- src/components/TodoDisplay.tsx
+++ src/components/TodoDisplay.tsx
@@ -1,13 +1,35 @@
 import React from "react";
 import { useQuery } from "@apollo/client";
 import { GET_TODO } from "../mutations/mutations";
+import "../styles/TodoDisplay.css";
+import { UPDATE_TODO } from "../mutations/mutations";
 
 const TodoDisplay = () => {
   const todoProps = useQuery(GET_TODO);
 
-  console.log(todoProps.data);
+//   console.log(todoProps.data);   
 
-  return <></>;
+  return (
+    <>
+      <div className="todoDisplayBox">
+        {todoProps.data &&
+          todoProps.data.todos.map((item: any, i: number) => {
+            return (
+              <div className="todoDataBox">
+                <h4 className="indexBox">{i}</h4>
+                <h4 className="colonBox">:</h4>
+                <h4 className="idBox">ID - {item.id}</h4>
+                <h4 className="colonBox">:</h4>
+                <input className="todoInputBox" defaultValue={item.type} />
+                <button className="todoInputBox" type="button">
+                  Update Todo
+                </button>
+              </div>
+            );
+          })}
+      </div>
+    </>
+  );
 };
 
 export default TodoDisplay;
