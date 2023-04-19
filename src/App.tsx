59450835e46f85cefd57e676268e934d8abe46ee
ts-geek43 <manish.prajapati@techstaunch.com>
2023-04-19 13:07:01 +0530

    Todo display added

--- src/App.tsx
+++ src/App.tsx
@@ -2,6 +2,7 @@ import React from "react";
 import logo from "./logo.svg";
 import "./App.css";
 import AddTodo from "./components/AddTodo";
+import TodoDisplay from "./components/TodoDisplay";
 
 function App() {
   return (
@@ -9,6 +10,7 @@ function App() {
       <h1> Apollo Client Mutation Practice</h1>
 
       <AddTodo />
+      <TodoDisplay />
     </div>
   );
 }
