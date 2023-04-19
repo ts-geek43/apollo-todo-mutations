3c0102e93e038595b32ad7d66a083f5aaed7aaf7
ts-geek43 <manish.prajapati@techstaunch.com>
2023-04-19 10:48:36 +0530

    Apollo client setted and AddTodo component added

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,14 @@
 import React from "react";
 import logo from "./logo.svg";
 import "./App.css";
+import AddTodo from "./components/AddTodo";
 
 function App() {
   return (
     <div className="App-header">
       <h1> Apollo Client Mutation Practice</h1>
+
+      <AddTodo />
     </div>
   );
 }
--- src/index.tsx
+++ src/index.tsx
@@ -1,15 +1,23 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+import React from "react";
+import ReactDOM from "react-dom/client";
+import "./index.css";
+import App from "./App";
+import reportWebVitals from "./reportWebVitals";
+import { ApolloClient, InMemoryCache, ApolloProvider } from "@apollo/client";
+
+const TodoClient = new ApolloClient({
+  uri: "https://sxewr.sse.codesandbox.io/",
+  cache: new InMemoryCache(),
+});
 
 const root = ReactDOM.createRoot(
-  document.getElementById('root') as HTMLElement
+  document.getElementById("root") as HTMLElement
 );
 root.render(
   <React.StrictMode>
-    <App />
+    <ApolloProvider client={TodoClient}>
+      <App />
+    </ApolloProvider>
   </React.StrictMode>
 );
 
