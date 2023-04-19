e8a491ca31dc4eed40365e674fa9456200ed731a
ts-geek43 <manish.prajapati@techstaunch.com>
2023-04-19 13:06:12 +0530

    Add to do done with manually updating the cachinh

--- src/components/AddTodo.tsx
+++ src/components/AddTodo.tsx
@@ -1,10 +1,27 @@
-import { useMutation } from "@apollo/client";
+import { gql, useMutation } from "@apollo/client";
 import React, { useRef } from "react";
 import "../styles/AddTodo.css";
-import { ADD_TODO } from "../mutations/mutations";
+import { ADD_TODO, FRAGMENT_TODO } from "../mutations/mutations";
+import TodoDisplay from "./TodoDisplay";
 
 const AddTodo = () => {
-  const [addTodo, addTodoProps] = useMutation(ADD_TODO);
+  //   const [dummy, setDummy] = useState(true);
+
+  const [addTodo, addTodoProps] = useMutation(ADD_TODO, {
+    update(cache, { data: { addTodo } }) {
+      cache.modify({
+        fields: {
+          todos(existingTodos = []) {
+            existingTodos = [...existingTodos, addTodo];
+            return existingTodos;
+          },
+        },
+      });
+    },
+  });
+
+//   const [addTodo, addTodoProps] = useMutation(ADD_TODO);
+
   const inputTodoRef = useRef<HTMLInputElement>(null);
 
   return (
@@ -23,6 +40,8 @@ const AddTodo = () => {
           onClick={() => {
             console.log(inputTodoRef.current?.value);
             addTodo({ variables: { type: inputTodoRef.current?.value } });
+            inputTodoRef.current?.setAttribute("value", "");
+            // setDummy(!dummy);
           }}
         >
           Add Todo
