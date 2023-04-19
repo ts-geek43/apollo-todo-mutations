8f71e0400d357163b28813dfe5ca8273ff58f408
ts-geek43 <manish.prajapati@techstaunch.com>
2023-04-19 13:04:22 +0530

    mutations and queries written with fragments

--- src/mutations/mutations.ts
+++ src/mutations/mutations.ts
@@ -1,19 +1,38 @@
 import { gql } from "@apollo/client";
 
+export const FRAGMENT_TODO = gql`
+  fragment TodoParts on Todo {
+    id
+    type
+  }
+`;
+
 export const ADD_TODO = gql`
+  ${FRAGMENT_TODO}
   mutation AddTodo($type: String!) {
     addTodo(type: $type) {
       id
-      type
+      ...TodoParts
     }
   }
 `;
 
 export const GET_TODO = gql`
+  ${FRAGMENT_TODO}
   query getTodo {
     todos {
       id
-      type
+      ...TodoParts
+    }
+  }
+`;
+
+export const UPDATE_TODO = gql`
+  ${FRAGMENT_TODO}
+  mutation UpdateTodo($id: String!, $type: String!) {
+    updateTodo(id: $id, type: $type) {
+      id
+      ...TodoParts
     }
   }
 `;
