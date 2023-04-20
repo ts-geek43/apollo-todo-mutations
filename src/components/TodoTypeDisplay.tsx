006a2db7f71f49dc527cf96c51681c54218e779a
ts-geek43 <manish.prajapati@techstaunch.com>
2023-04-20 10:57:39 +0530

    Todo type display refactored

--- src/components/TodoTypeDisplay.tsx
+++ src/components/TodoTypeDisplay.tsx
@@ -5,13 +5,20 @@ import "../styles/TodoDisplay.css";
 
 const TodoTypeDisplay = () => {
   const [loadTodoLazy, lazyTodoProps] = useLazyQuery(GET_TODO);
-
+  console.log("Lazily loaded data: - ", lazyTodoProps.data?.todos);
   return (
     <React.Fragment>
       <div>
         <button className="todoInputBox" onClick={() => loadTodoLazy()}>
           Load Lazily
         </button>
+
+        {lazyTodoProps.loading && <h2>Loading....</h2>}
+        {lazyTodoProps.error && <h2>Error....</h2>}
+        {lazyTodoProps.data &&
+          lazyTodoProps.data?.todos.map((item: any) => {
+            return <h5>{item.type}</h5>;
+          })}
       </div>
     </React.Fragment>
   );
